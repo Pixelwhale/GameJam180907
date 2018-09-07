cdbf5604ec60ad4f578494638ba78b3d217e8cad
Pixelwhale <30408629+Pixelwhale@users.noreply.github.com>
2018-09-07 10:54:59 +0900

    Note and Player

---
+++ GameJam 2018.9/Assets/Script/GamePlay/HP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HP : MonoBehaviour
+{
+    private int m_hp;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_hp = 200;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Damaged(int damage)
+    {
+        m_hp -= damage;
+		Death();
+    }
+
+    private void Death()
+    {
+        if (m_hp <= 0)
+        {
+            //gameover
+        }
+    }
+}
---
+++ GameJam 2018.9/Assets/Script/GamePlay/Note/NoteBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBase : MonoBehaviour
+{
+    private NoteType m_noteType;
+    private Vector2 m_position;
+    private int m_bpm;
+    private int m_dmg;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OverRange();
+    }
+
+    //death animation
+    void Death()
+    {
+
+    }
+
+    void OverRange()
+    {
+        if (m_position.x <= -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
