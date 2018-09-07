6ea075587abb63b6312a098923a74b94050cf414
リン <linjerry0906@gmail.com>
2018-09-07 09:24:07 +0900

    [update] SceneEnum

--- GameJam 2018.9/Assets/Script/GameManager/System/Scene/SceneEnum.cs
+++ GameJam 2018.9/Assets/Script/GameManager/System/Scene/SceneEnum.cs
@@ -8,21 +8,9 @@ public enum SceneEnum
 {
 	Logo = 0,				//Logo
 	Title,					//Title
-	Home,					//拠点
-	Unit_Main,				//Unit Main 画面
-	Unit_Team,				//チーム画面
-	Unit_LevelUp,			//強化画面
-	Quest_Main,				//Quest Main 画面
-	Quest_Chapter,			//Chapter選択画面
-	Quest_Character,		//Character Chapter画面
-	Quest_Story,			//Story
-	Quest_Game,				//Game Main
-	Quest_Result,			//Game Result
-	Shop,					//Shop
-	Gallery_Main,			//Gallery　Main画面
-	Gallery_Music,			//音楽鑑賞
-	Gallery_Art,			//絵画鑑賞
-	Gallery_Character,		//キャラ図鑑
+	Music_Select,
+	GamePlay,
+	Result,
 	Option,					//Option
 	Null,					//Last One
 }
