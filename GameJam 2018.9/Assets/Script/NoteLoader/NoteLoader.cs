b441e3b59fb07feac155770a025206145064dc5a
リン <linjerry0906@gmail.com>
2018-09-07 10:48:09 +0900

    [add] NoteLoader, NoteType

---
+++ GameJam 2018.9/Assets/Script/NoteLoader/NoteLoader.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------
+// 作成日：2018.9.7
+// 作成者：林 佳叡
+// 内容：NoteLoader
+//------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class NoteLoader : MonoBehaviour 
+{
+	public void LoadNotesFile(string fileName, ref List<string> result, ref int bpm)
+	{
+		FileStream fs = new FileStream("Resources/csv/" + fileName, FileMode.Open, FileAccess.Read);
+		StreamReader sr = new StreamReader(fs);
+
+		result = new List<string>();
+
+		string bpmString = sr.ReadLine();				//First line
+		bpm = int.Parse(bpmString.Split(',')[1]);		//BPM取得
+
+		while(!sr.EndOfStream)
+		{
+			string line = sr.ReadLine();				//読み込む
+			result.Add(line);							//List追加
+		}
+
+		sr.Close();
+		fs.Close();
+	}
+}
---
+++ GameJam 2018.9/Assets/Script/NoteLoader/NoteType.cs
@@ -0,0 +1,13 @@
+﻿//------------------------------------------------------
+// 作成日：2018.9.7
+// 作成者：林 佳叡
+// 内容：Note列挙型
+//------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NoteType
+{
+
+}
