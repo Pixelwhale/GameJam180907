6aa246c9369183f005e031cf26fe47c0b105b9df
Kimsunwon <flypig1208@gmail.com>
2018-09-11 10:31:22 +0900

    Perfectを判断する距離クラス追加、　TapNoteの修正

---
+++ GameJam 2018.9/Assets/Script/GamePlay/Note/Check/Distance_Perfect.cs
@@ -0,0 +1,12 @@
+﻿//2018.09.11 金　淳元
+//Perfectを判定する距離クラス
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance_Perfect : MonoBehaviour {
+
+    [Header("Perfectを判断する距離")]
+    public static readonly float distance_perfect = 0.2f;
+}
