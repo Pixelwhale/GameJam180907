18a1c096f387db7cc6204fb98befcd88d6b0d399
Kimsunwon <flypig1208@gmail.com>
2018-09-11 11:43:36 +0900

    MissBoxのprefabとscript追加

---
+++ GameJam 2018.9/Assets/Script/GamePlay/Note/Check/MissBox.cs
@@ -0,0 +1,16 @@
+﻿//2018.09.11 金　淳元　
+//MissBoxクラス
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissBox : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            //Todo: scoreManagerにMissをreturn；
+        }
+    }
+}
