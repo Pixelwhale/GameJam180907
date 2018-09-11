1b6d5eeb8066891f11b944a148c481252ea2c21a
リン <linjerry0906@gmail.com>
2018-09-11 15:44:37 +0900

    [add] checkPoint Animation

---
+++ GameJam 2018.9/Assets/Script/GamePlay/PlaySystem/CheckPointAnime.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------
+// 作成日：2018.9.10
+// 作成者：林 佳叡
+// 内容：一発Note
+//------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointAnime : MonoBehaviour 
+{
+	private Animator checkpointAnime;
+	void Start () 
+	{
+		checkpointAnime = GetComponent<Animator>();
+		checkpointAnime.SetBool("isPlay", false);
+	}
+
+	public void PlayAnimation(int bpm)
+	{
+		checkpointAnime.speed = bpm / 60.0f;
+		checkpointAnime.SetBool("isPlay", true);
+	}
+}
